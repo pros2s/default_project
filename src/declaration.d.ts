719512b0d168f1f8626b363f3a7c125b1296e7ab
Alexandr <alexandrpod22@gmail.com>
2026-03-11 17:09:08 +0300

    png format declaration

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -11,7 +11,10 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
-
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
 declare module '*.css' {
   const content: Record<string, string>;
   export default content;
